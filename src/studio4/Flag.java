1542d7c1e7c3b6636d344871b34f3d46c9e31596
spencerlambert <spencerlambert@localhost>
2023-10-04 14:17:50 -0500

    studio

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,23 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+	
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
+		
+		StdDraw.clear();
+		StdDraw.setPenColor(200, 90, 90);
+		StdDraw.filledRectangle(0, 4, 1, 1);
+		StdDraw.filledRectangle(18, 22 , 1, 1);
+		   
+		StdDraw.setPenColor(30, 190, 100);
+		StdDraw.filledRectangle(0, 22, 1, 1);
+		  
+		}
+	
+		
+		
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,31 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		//String[] array= new String[10];
+		//for (int i=0; i<10; i+=1) {
+		//	array[i]=in.nextLine();
+		//}
+		
+		//double x = (double) array[6];
+		//double y = (double) array[7];
+		//double width = (double) array[8];
+		//double heigh = (double) array[9];
+		
+		
+		String second = in.next();
+		int third = in.nextInt();
+		int fourth = in.nextInt();
+		int fifth = in.nextInt();
+		boolean sixth = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double width = in.nextDouble();
+		double heigh = in.nextDouble();
+				
+				
+		StdDraw.rectangle(x, y, width, heigh);
+		
+		
+		
 	}
 }
